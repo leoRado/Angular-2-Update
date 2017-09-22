b489ac7bb880e4f3202401c5166395109ec19406
Leonhard Radonic <leonhard.radonic@gmail.com>
2017-09-22 16:55:40 +0200

    Final release Angular-2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+  template: `<h1>Hello World</h1>
+  <sub-app></sub-app>
+  `
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { SubComponent } from './16.sub.component';
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SubComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
