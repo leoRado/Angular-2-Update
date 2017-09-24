bee4f7952fd59cd37db9319ed0cf37f67483a358
Leo <leonhard.radonic@gmail.com>
2017-09-24 17:57:42 +0200

    Model driven forms (22, 23th)

---
+++ app/22.mdf.validate.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/22.mdf.validate.html',
+  styles: [`input.ng-invalid {border-left: 5px solid red;}
+            input.ng-valid {border-left: 5px solid green;}
+  `]
+})
+export class AppComponent {
+    userForm = new FormGroup({
+        name: new FormControl(null, [
+            Validators.required, 
+            Validators.minLength(4), 
+            Validators.maxLength(10)
+        ]),
+        email: new FormControl(),
+        address: new FormGroup({
+            street: new FormControl(),
+            city: new FormControl(),
+            postalCode: new FormControl(null, Validators.pattern('^[1-9][0-9]{4}'))
+        })
+    });
+
+    submitUser() {
+        console.log(this.userForm.value);
+    }
+ }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
-import { SubComponent } from './16.sub.component';
-import { FormsModule } from '@angular/forms';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { AppComponent }         from './22.mdf.validate';
+import { SubComponent }         from './16.sub.component';
+import { FormsModule }          from '@angular/forms';
+import { ReactiveFormsModule }  from '@angular/forms';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, SubComponent ],
   bootstrap:    [ AppComponent ]
 })
